9eb3a6b1c81fc17045833a528845ef50cc0dbec6
liyujiang <liyujiang_tk@yeah.net>
2015-05-27 22:05:57 +0800

    更换地图

--- src/com/gztoucher/MapSlicingTool/Config.java
+++ src/com/gztoucher/MapSlicingTool/Config.java
@@ -1,16 +1,17 @@
 package com.gztoucher.MapSlicingTool;
 
 public class Config {
-	public static String imgPath = "E:/workspace/MapSlicingTool/map.jpg";// 待裁剪的图片
-	public static String saveDirPath = "E:/";// 裁剪后的保存路径
-	public static String name = "map";
-	public static int tileSize = 256;// 瓦片的大小
-	public static int pointTopLeftX = 180;// 左上角屏幕X坐标
-	public static int pointTopLeftY = 200;// 左上角屏幕Y坐标
-	public static double pointTopLeftLat = 26.4428;// 左上角纬度
-	public static double pointTopLeftLon = 106.6738;// 左上角经度
-	public static int pointBottomRightX = 900;// 右下角屏幕X坐标
-	public static int pointBottomRightY = 700;// 右下角屏幕Y坐标
-	public static double pointBottomRightLat = 26.6324;// 右下角纬度
-	public static double pointBottomRightLon = 106.5553;// 右下角经度
+    public static String imgPath = "E:/workspace/MapSlicingTool/map.jpg";// 待裁剪的图片
+    public static String saveDirPath = "E:/workspace/MapSlicingTool/";// 裁剪后的保存路径
+    public static String name = "map";
+    public static int tileSize = 256;// 瓦片的大小
+    public static int pointTopLeftX = 20;// 左上角屏幕X坐标
+    public static int pointTopLeftY = 28;// 左上角屏幕Y坐标
+    //坐标拾取器：http://api.map.baidu.com/lbsapi/getpoint/，如核桃冲：105.178774,26.824964
+    public static double pointTopLeftLat = 26.846441;// 左上角纬度//
+    public static double pointTopLeftLon = 105.129304;// 左上角经度
+    public static int pointBottomRightX = 1348;// 右下角屏幕X坐标
+    public static int pointBottomRightY = 389;// 右下角屏幕Y坐标
+    public static double pointBottomRightLat = 26.823279;// 右下角纬度
+    public static double pointBottomRightLon = 105.2243;// 右下角经度
 }
--- src/com/gztoucher/MapSlicingTool/Main.java
+++ src/com/gztoucher/MapSlicingTool/Main.java
@@ -6,41 +6,41 @@ import com.ls.mappwidget.slicingtool.cutter.OnProgressUpdateListener;
 import com.ls.mappwidget.slicingtool.vo.PointVO;
 
 /**
- * 地图裁剪工具。 详见：https://github.com/lemberg/mappwidget
- * 
- * @author liyujiang
- * 
+ * 地图裁剪工具。
+ * 详见：https://github.com/lemberg/mappwidget
+ *
+ * @author liyujiang<QQ:1032694760>
  */
 public class Main {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		Cutter cutter = new Cutter(new OnProgressUpdateListener() {
-			@Override
-			public void onProgressUpdate(int value) {
-				System.out.println("progress: " + value);
-			}
-		}, new OnCompliteListener() {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Cutter cutter = new Cutter(new OnProgressUpdateListener() {
+            @Override
+            public void onProgressUpdate(int value) {
+                System.out.println("progress: " + value);
+            }
+        }, new OnCompliteListener() {
 
-			@Override
-			public void onComplite() {
-				System.out.println("finish");
-			}
-		});
-		PointVO pointTopLeft = new PointVO();
-		pointTopLeft.setX(Config.pointTopLeftX);
-		pointTopLeft.setY(Config.pointTopLeftY);
-		pointTopLeft.setLat(Config.pointTopLeftLat);
-		pointTopLeft.setLon(Config.pointTopLeftLon);
-		PointVO pointBottomRight = new PointVO();
-		pointBottomRight.setX(Config.pointBottomRightX);
-		pointBottomRight.setY(Config.pointBottomRightY);
-		pointBottomRight.setLat(Config.pointBottomRightLat);
-		pointBottomRight.setLon(Config.pointBottomRightLon);
-		cutter.startCuttingAndroid(Config.imgPath, Config.saveDirPath,
-				Config.name, Config.tileSize, pointTopLeft, pointBottomRight);
-	}
+            @Override
+            public void onComplite() {
+                System.out.println("finish");
+            }
+        });
+        PointVO pointTopLeft = new PointVO();
+        pointTopLeft.setX(Config.pointTopLeftX);
+        pointTopLeft.setY(Config.pointTopLeftY);
+        pointTopLeft.setLat(Config.pointTopLeftLat);
+        pointTopLeft.setLon(Config.pointTopLeftLon);
+        PointVO pointBottomRight = new PointVO();
+        pointBottomRight.setX(Config.pointBottomRightX);
+        pointBottomRight.setY(Config.pointBottomRightY);
+        pointBottomRight.setLat(Config.pointBottomRightLat);
+        pointBottomRight.setLon(Config.pointBottomRightLon);
+        cutter.startCuttingAndroid(Config.imgPath, Config.saveDirPath,
+                Config.name, Config.tileSize, pointTopLeft, pointBottomRight);
+    }
 
 }
